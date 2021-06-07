04eb909c61df42d2851bcefd8db53cc03762eaf0
TMax66 <massimo.tranquillo@gmail.com>
2021-06-08 00:29:59 +0200

    update

--- codici.R
+++ codici.R
@@ -2,6 +2,7 @@ library("tidyverse")
 library("readxl")
 library("here")
 library("lubridate")
+library("zoo")
 
 dati <- read_excel("data/Dati epidemiogici RV_2016-2019-070621.xlsx")
 
@@ -15,4 +16,29 @@ dt <- dati %>%
          Month = month(dtprelievo), 
          Week = week(dtprelievo)) 
 
-         
+        
+dt %>% 
+  group_by(Year,Month, RV) %>% 
+  summarise(n= n()) %>% 
+  pivot_wider(names_from = RV, values_from = n, values_fill = 0) %>% 
+  mutate(Prev = P/(P+N)) %>%
+  ggplot(aes(x = Month, y= Prev))+
+  geom_line()
+
+
+dt %>% 
+  group_by(dtprelievo,  RV) %>% 
+  summarise(n= n()) %>% 
+  pivot_wider(names_from = RV, values_from = n, values_fill = 0) %>% 
+  mutate(Prev = P/(P+N)) %>% 
+  ggplot(aes(x = dtprelievo, y= Prev))+
+  geom_line()
+
+
+dt %>% filter(RVA == "P" & RVH == "P") %>% 
+  count(M = floor_date(dtprelievo, "month")) %>% 
+  mutate(m = rollmean(n, k = 3, fill = NA) )%>%   
+  ggplot(aes(M, n))+
+  geom_line()+
+  geom_line(aes(x=M, y=m), col = "blue", size = 1.5)
+  
